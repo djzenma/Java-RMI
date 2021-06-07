56b19ecba74d3f1458751724b2c2a29f23233ffd
djzenma <eidma@aucegypt.edu>
2021-06-06 22:58:49 -0400

    Added logging for Client

--- src/com/company/ManagerClient.java
+++ src/com/company/ManagerClient.java
@@ -1,12 +1,17 @@
 package com.company;
 
+import com.company.record.StudentRecord;
+import com.company.record.TeacherRecord;
 import com.company.rmi.CenterServer;
 import com.company.types.Location;
 import com.company.types.Status;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -16,6 +21,7 @@ public class ManagerClient {
     private String managerID;
     private CenterServer ops;
     private Location location;
+    private FileWriter writer;
 
     public ManagerClient(int serverRegistryPort) {
         try {
@@ -33,7 +39,31 @@ public class ManagerClient {
                                  String phone,
                                  String specialization,
                                  Location location) throws RemoteException {
-        return ops.createTRecord(firstName, lastName, address, phone, specialization, location);
+        TeacherRecord templateTR = new TeacherRecord(
+                "",
+                firstName,
+                lastName,
+                address,
+                phone,
+                specialization,
+                location);
+        try {
+            writeEvent("ISSUED CREATE TEACHER RECORD: " + templateTR.print());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        boolean isCreated = ops.createTRecord(firstName, lastName, address, phone, specialization, location);
+
+        try {
+            if (isCreated)
+                writeEvent("SUCCESS: TEACHER RECORD CREATED: " + templateTR.print());
+            else
+                writeEvent("FAILURE: TEACHER RECORD NOT CREATED: " + templateTR.print());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isCreated;
     }
 
     public boolean createSRecord(String firstName,
@@ -41,28 +71,96 @@ public class ManagerClient {
                                  ArrayList<String> courseRegistered,
                                  Status status,
                                  Date statusDate) throws RemoteException {
-        return ops.createSRecord(firstName, lastName, courseRegistered, status, statusDate);
-    };
+        StudentRecord templateSR = new StudentRecord(
+                "",
+                firstName,
+                lastName,
+                courseRegistered,
+                status,
+                statusDate);
+
+        try {
+            writeEvent("ISSUED CREATE STUDENT RECORD: " + templateSR.print());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        boolean isCreated = ops.createSRecord(firstName, lastName, courseRegistered, status, statusDate);
+
+        try {
+            if (isCreated)
+                writeEvent("SUCCESS: STUDENT RECORD CREATED: " + templateSR.print());
+            else
+                writeEvent("FAILURE: STUDENT RECORD NOT CREATED: " + templateSR.print());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isCreated;
+    }
+
+    ;
 
     public HashMap<Location, Integer> getRecordCounts() throws RemoteException {
-        return ops.getRecordCounts();
-    };
+        try {
+            writeEvent("ISSUED RECORDS COUNT");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HashMap<Location, Integer> count = ops.getRecordCounts();
+
+        try {
+            StringBuilder event = new StringBuilder();
+            for (Location loc : Location.values()) {
+                event.append(loc.name()).append(": ").append(count.get(loc)).append(", ");
+            }
+            writeEvent("SUCCESS: RECORDS COUNT: " + event);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    ;
 
     public boolean editRecord(String recordID, String fieldName, Object newValue) throws RemoteException {
-        return ops.editRecord(recordID, fieldName, newValue);
-    };
+        try {
+            writeEvent("ISSUED EDIT RECORD: update the field \"" + fieldName + "\" to " + newValue.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        boolean isEdited = ops.editRecord(recordID, fieldName, newValue);
+
+        try {
+            if (isEdited)
+                writeEvent("SUCCESS: RECORD EDITED: updated the field \"" + fieldName + "\" of the record with recordID \"" + recordID + "\" to " + newValue.toString());
+            else
+                writeEvent("FAILURE: RECORD NOT EDITED: did not update the field \"" + fieldName + "\" of the record with recordID \"" + recordID + "\" to " + newValue.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isEdited;
+    }
+
+    ;
 
     public Location getLocation() {
-        if(managerID.startsWith(Location.MTL.name()))
+        if (managerID.startsWith(Location.MTL.name()))
             return Location.MTL;
-        if(managerID.startsWith(Location.LVL.name()))
+        if (managerID.startsWith(Location.LVL.name()))
             return Location.LVL;
-        if(managerID.startsWith(Location.DDO.name()))
+        if (managerID.startsWith(Location.DDO.name()))
             return Location.DDO;
         return null;
     }
 
     public void setManagerID(String managerID) {
         this.managerID = managerID;
+    }
+
+    private synchronized void writeEvent(String event) throws IOException {
+        writer = new FileWriter("log/client/" + managerID + ".txt", true);
+        writer.write(new Timestamp(new Date().getTime()).toString() + " " + event + "\n");
+        writer.close();
     }
 }
---
+++ src/com/company/utils/Logger.java
@@ -0,0 +1,12 @@
+package com.company.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Logger {
+    private FileWriter writer;
+
+
+}
